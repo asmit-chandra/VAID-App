83439d21eada999e62be7afaab83837533638e8e
Asmit Chandra <asmitchandra@yahoo.com>
2021-05-23 20:32:12 +0530

    minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
+var pop;
+
 function setup() {
+	pop = new Audio('pop.mp3');
+	chatbot.replyDelay = 500;
 	chatbot.getDB('1QVQC1DZN5drhhawbIwrl3WSJO6c99JYiyXPxT4Ur6sU').then(data => {
-		chatbot.db = data;
+	chatbot.db = data;
 	chatbot.loadFiles(['bot.rive']);
 });
 }
